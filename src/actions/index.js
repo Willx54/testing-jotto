54c2bfa2bd45ddb47a229237e1b25ce1e769e3fe
Will <w_priddle@hotmail.com>
2020-04-16 08:37:12 +0100

    Add Thunk for 'guessedWords' piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,5 +14,14 @@ export const actionTypes = {
 export const guessWord = (guessedWord) => {
   return function (dispatch, getState) {
     const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount },
+    });
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
   };
 };
