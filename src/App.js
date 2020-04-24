f7c5cca398e8716f0d013b0b0cad3d3de1c5fb24
Will <w_priddle@hotmail.com>
2020-04-24 08:47:54 +0100

    Basic Input component.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import "./App.css";
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
 
-class App extends Component {
-  render() {
-    return <div data-test="component-app"></div>;
-  }
+function App() {
+  return <div data-test="component-app"></div>;
 }
 
 export default App;
