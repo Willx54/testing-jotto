c82d1caec177579c0e24ad16b7109c7bd2a8626f
Will <w_priddle@hotmail.com>
2020-04-24 08:35:46 +0100

    Basic app component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ import Congrats from "./Congrats";
 
 class App extends Component {
   render() {
-    return (
-      <div className="container">
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
-      </div>
-    );
+    return <div data-test="component-app"></div>;
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import Enzyme from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
-
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+/**
+ * Setup function for app component.
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
 
-test("renders learn react link", () => {});
+test("App renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-app");
+  expect(component.length).toBe(1);
+});
