70713b9775623605a6edc88633d50a25705079ba
Will <w_priddle@hotmail.com>
2020-04-28 08:54:35 +0100

    Test state updates with value of input box upon change

--- src/Input.test.js
+++ src/Input.test.js
@@ -22,3 +22,18 @@ test("does not throw warning with expected props", () => {
   const expectedProps = { secretWord: "party" };
   checkProps(Input, expectedProps);
 });
+
+describe("state controlled input field", () => {
+  test("state updates with value of input box upon change", () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+});
