e282941063a818af189928400cc65b2392d7faf7
Will <w_priddle@hotmail.com>
2020-04-30 09:00:27 +0100

    Work in preogress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import GuessedWords from "./GuessedWords";
-import Congrats from "./Congrats";
+/**
+ * reducer to update state
+ * state {object} - existing state
+ * action {object} - contains 'type' and 'payload' properties for the state update
+ *                    for example: { type: "setSecretWord", payload: "party" }
+ */
+function reducer(state, action) {}
 
 function App() {
   return <div data-test="component-app"></div>;
