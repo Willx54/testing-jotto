30b76e1bef8e0fbf8f16a50260998e3e7e3c008a
Will <w_priddle@hotmail.com>
2020-05-15 08:50:17 +0100

    Some problems still

--- src/LanguagePicker.js
+++ src/LanguagePicker.js
@@ -2,7 +2,20 @@ import React from "react";
 import propTypes from "prop-types";
 
 function LanguagePicker({ setLanguage }) {
-  return <div data-test="component-language-picker"></div>;
+  const languages = [
+    { code: "en", symbol: "&#127468" },
+    { code: "cn", symbol: "&#127464" },
+  ];
+  const languageIcons = languages.map((lang) => (
+    <span
+      data-test="language-icon"
+      key={lang.code}
+      onClick={() => setLanguage(lang.code)}
+    >
+      {lang.symbol}
+    </span>
+  ));
+  return <div data-test="component-language-picker">{languageIcons}</div>;
 }
 
 export default LanguagePicker;
